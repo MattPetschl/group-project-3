92e2e047466c9e3e5201e970e52d464f464831df
Matthew Petschl <matthew.petschl@gmail.com>
2019-07-05 21:54:09 -0500

    wip :)

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,54 @@
+require("dotenv").config();
 const express = require("express");
-
 const mongoose = require("mongoose");
-const routes = require("./routes");
+const cors = require("cors");
+
 const app = express();
-const PORT = process.env.PORT || 3001;
+const emailController = require("./controllers/email/email.controller");
+const { PORT, CLIENT_ORIGIN, DB_URL } = require("./config");
 
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
+// Only allow requests from our client
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN
+  })
+);
+
+// Allow the app to accept JSON on req.body
 app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-// Add routes, both API and view
-app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+// This endpoint is pinged every 5 mins by uptimerobot.com to prevent
+// free services like Heroku and Now.sh from letting the app go to sleep.
+// This endpoint is also pinged every time the client starts in the
+// componentDidMount of App.js. Once the app is confirmed to be up, we allow
+// the user to perform actions on the client.
+app.get("/wake-up", (req, res) => res.json("👌"));
 
-// Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+// This is the endpoint that is hit from the onSubmit handler in Landing.js
+// The callback is shelled off to a controller file to keep this file light.
+app.post("/email", emailController.collectEmail);
+
+// Same as above, but this is the endpoint pinged in the componentDidMount of
+// Confirm.js on the client.
+app.get("/email/confirm/:id", emailController.confirmEmail);
+
+// Catch all to handle all other requests that come into the app.
+app.use("*", (req, res) => {
+  res.status(404).json({ msg: "Not Found" });
 });
+
+// To get rid of all those semi-annoying Mongoose deprecation warnings.
+const options = {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useFindAndModify: false
+};
+
+// Connecting the database and then starting the app.
+mongoose
+  .connect(DB_URL, options, () => {
+    app.listen(PORT, () => console.log("👍"));
+  })
+  // The most likely reason connecting the database would error out is because
+  // Mongo has not been started in a separate terminal.
+  .catch(err => console.log(err));
