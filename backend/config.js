0bea7b4bbc84d59a451dce9e6ce4a0c4ccdd3e33
Matthew Petschl <matthew.petschl@gmail.com>
2019-07-09 22:42:15 -0500

    changed mongo db name

--- backend/config.js
+++ backend/config.js
@@ -4,8 +4,7 @@ exports.CLIENT_ORIGIN =
   process.env.NODE_ENV === "production"
     ? process.env.CLIENT_ORIGIN
     : "http://localhost:3000";
-console.log(process.env);
 exports.DB_URL =
   process.env.NODE_ENV === "production"
     ? process.env.DB_URL
-    : "mongodb://localhost/simple-email-confirmation";
+    : "mongodb://localhost/amass";
