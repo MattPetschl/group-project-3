6bb7a657150824741b57e4d499dfc97cc673c5b5
Matthew Petschl <matthew.petschl@gmail.com>
2019-07-09 22:43:45 -0500

    updated frontend api routes

--- frontend/src/utils/API.js
+++ frontend/src/utils/API.js
@@ -2,19 +2,20 @@ import axios from "axios";
 
 export default {
   // Gets all events
-  getEvent: function() {
+  getEvents: function() {
     return axios.get("/api/events");
   },
   // Gets the events with the given id
   getEvent: function(id) {
-    return axios.get("/api/event/" + id);
+    return axios.get("/api/events/" + id);
   },
   // Deletes the events with the given id
   deleteEvent: function(id) {
-    return axios.delete("/api/event/" + id);
+    return axios.delete("/api/events/" + id);
   },
   // Saves a book to the database
   saveEvent: function(eventData) {
-    return axios.post("/api/event", eventData);
+    console.log(eventData);
+    return axios.post("/api/events/", eventData);
   }
 };
